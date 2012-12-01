31243b9e9be483c22e5e3fdf16a32cb166930431
Changweihua <Changweihua@outlook.com>
2012-12-01 21:43:02 +0800

    企业库，帮助类，简化数据操作以及重复利用代码

---
+++ EnterpriseObjects/XmlHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnterpriseObjects
+{
+    /*************************************************************************************
+     * CLR 版本:       4.0.30319.586
+     * 类 名 称:       XmlHelper
+     * 机器名称:       HSERVER
+     * 命名空间:       EnterpriseObjects
+     * 文 件 名:       XmlHelper
+     * 创建时间:       2012/12/1 21:16:33
+     * 作    者:       常伟华 Changweihua
+     * 签    名:       To be or not, it is not a problem !
+     * 网    站:       http://www.cmono.net
+     * 邮    箱:       changweihua@outlook.com
+     * 
+     * 修改时间:
+     * 修 改 人:
+     * 
+     ************************************************************************************/
+    /// <summary>
+    /// Xml文件帮助类
+    /// </summary>
+    public class XmlHelper
+    {
+        public void sss() 
+        {
+
+        }
+    }
+}
