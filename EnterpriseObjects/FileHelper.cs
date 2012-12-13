f9a95b6a64fcd9c02343df0f6bd06b3568c79eab
Changweihua <changweihua@outlook.com>
2012-12-13 22:13:58 +0800

    文件帮助类

--- EnterpriseObjects/FileHelper.cs
+++ EnterpriseObjects/FileHelper.cs
@@ -39,14 +39,30 @@ namespace EnterpriseObjects
 
             if (File.Exists(filePath))
             {
-                try
+                if (File.GetAttributes(filePath) == FileAttributes.Normal)
                 {
-                    File.Delete(filePath);
-                    flag = true;
+                    try
+                    {
+                        File.Delete(filePath);
+                        flag = true;
+                    }
+                    catch
+                    {
+                        flag = false;
+                    }
                 }
-                catch
+                else
                 {
-                    flag = false;
+                    try
+                    {
+                        File.SetAttributes(filePath, FileAttributes.Normal);
+                        File.Delete(filePath);
+                        flag = true;
+                    }
+                    catch
+                    {
+                        flag = false;
+                    }
                 }
             }
             else
@@ -57,6 +73,51 @@ namespace EnterpriseObjects
             return flag;
         }
 
+
+        /// <summary>
+        /// 获取文件名
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static string GetFileName(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                FileInfo file = new FileInfo(filePath);
+                return file.Name;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// 获取文件名
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="includeExtension">是否包含拓展名</param>
+        /// <returns></returns>
+        public static string GetFileName(string filePath,bool includeExtension)
+        {
+            if (File.Exists(filePath))
+            {
+                FileInfo file = new FileInfo(filePath);
+                if (includeExtension)
+                {
+                    return file.Name;
+                }
+                else
+                {
+                    return file.Name.Replace(file.Extension,"");
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
 
         /// <summary>
         /// 移动文件
@@ -92,8 +153,47 @@ namespace EnterpriseObjects
             return flag;
         }
 
+
         /// <summary>
-        /// 检查否见是否存在
+        /// 获取文件拓展名
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public string GetFileExtension(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                return fileInfo.Extension;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// 打开文件
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public bool OpenFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                System.Diagnostics.Process.Start(filePath);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// 检查文件是否存在
         /// </summary>
         /// <param name="filePath"></param>
         /// <returns></returns>
@@ -106,6 +206,158 @@ namespace EnterpriseObjects
             }
             return flag;
         }
+
+
+        /// <summary>
+        /// 得到文件的大小
+        /// </summary>
+        /// <param name="fileFullPath">文件全路径</param>
+        /// <returns>string</returns>
+        public string GetFileLength(string fileFullPath)
+        {
+            if (File.Exists(fileFullPath))
+            {
+                FileInfo fileInfo = new FileInfo(fileFullPath);
+                long f = fileInfo.Length;
+                //KB MB GB TB
+                if (f > 1024 * 1024 * 1024)//GB
+                {
+                    return Convert.ToString(Math.Round((f + 0.00) / (1024 * 1024 * 1024), 2)) + "GB";
+                }
+                if (f > 1024 * 1024) //MB
+                {
+                    return Convert.ToString(Math.Round((f + 0.00) / (1024 * 1024), 2)) + "MB";
+                }
+                else
+                {
+                    return Convert.ToString(Math.Round((f + 0.00) / 1024, 2)) + "KB";
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 文件转换成二进制流
+        /// </summary>
+        /// <param name="fileFullPath">要转换的文件全路径</param>
+        /// <returns> Byte[] </returns>
+        /// <remarks></remarks>
+        public Byte[] FileToStreamByte(string fileFullPath)
+        {
+            Byte[] fileData = null;
+            if (File.Exists(fileFullPath))
+            {
+                FileStream fs = new FileStream(fileFullPath, System.IO.FileMode.Open);
+                fileData = new Byte[fs.Length];
+                fs.Read(fileData, 0, fileData.Length);
+                fs.Close();
+                return fileData;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+
+        /// <summary>
+        /// 从二进制转换成文件
+        /// </summary>
+        /// <param name="createFileFullPath">要转换的文件全路径</param>
+        /// <param name="streamByte">二进制流</param>
+        /// <returns>True/False</returns>
+        /// <remarks>True/False</remarks>
+        public bool ByteSteramToFile(string createFileFullPath, Byte[] streamByte)
+        {
+            try
+            {
+                FileStream fs = null;
+                if (File.Exists(createFileFullPath)) //如果文件存在，先删除
+                {
+                    DeleteFile(createFileFullPath);
+                }
+                fs = File.Create(createFileFullPath);
+                fs.Write(streamByte, 0, streamByte.Length);
+                fs.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// 序列化Xml文件
+        /// </summary>
+        /// <param name="fileFullPath">要序列化的xml文件路径</param>
+        /// <returns>True/False</returns>
+        public bool SerializeXmlFile(string fileFullPath)
+        {
+            System.Data.DataSet ds = new System.Data.DataSet();
+            if (File.Exists(fileFullPath))
+            {
+                try
+                {
+                    ds.ReadXml(fileFullPath);
+                    FileStream fs = new FileStream(fileFullPath + ".tmp", FileMode.OpenOrCreate);
+                    System.Runtime.Serialization.Formatters.Binary.BinaryFormatter BF = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    BF.Serialize(fs, ds);//序例化ds,并将结果赋给fs
+                    fs.Close();
+                    DeleteFile(fileFullPath);//删除传入的xml文件
+                    File.Move(fileFullPath + ".tmp", fileFullPath);//将序列化后的文件改为原来的Xml文件
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// 反序列化Xml
+        /// </summary>
+        /// <param name="fileFullPath">反序列化的Xml文件路径</param>
+        /// <returns>True/False</returns>
+        /// <remarks></remarks>
+        public bool DescSerializeXmlFile(string fileFullPath)
+        {
+            if (File.Exists(fileFullPath))
+            {
+                try
+                {
+                    System.Data.DataSet ds = new System.Data.DataSet();
+                    FileStream fs = new FileStream(fileFullPath, FileMode.Open);//打开xml文件,将内容读入到流中
+                    System.Runtime.Serialization.Formatters.Binary.BinaryFormatter BF = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    object descSeriResult = BF.Deserialize(fs);//反序列化
+                    ((System.Data.DataSet)descSeriResult).WriteXml(fs + ".tmp");
+                    fs.Close();//关闭流
+                    DeleteFile(fileFullPath);//删除传入的xml文件
+                    File.Move(fileFullPath + ".tmp", fileFullPath);//将反序列化后的文件改名传入的xml文件
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
 
         #endregion
     }
